a24385e1089c013a54699c793265cc30edcbe781
Mateo Guisao Flórez <1039475395@itagui.loc>
2022-10-21 09:25:17 -0500

    Mini projects

---
+++ Proyecto 2/function.js
@@ -0,0 +1,16 @@
+function unselect() {
+    document.querySelectorAll("[type='radio']");
+    radios.forEach((x)=>{
+        x.dataset.val = x.checked; // guardamos el estado del radio button dentro del elemento
+        x.addEventListener('click',(e)=>{
+          let element = e.target;
+          if(element.dataset.val == 'false'){
+            element.dataset.val = 'true';
+            element.checked = true;
+          }else{
+            element.dataset.val = 'false';
+            element.checked = false;
+          }
+        },true);
+    });
+}
